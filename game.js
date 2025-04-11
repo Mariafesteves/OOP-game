d5426624d9bfe26ef93fb6e8ff319e9738f1a682
Mariaesteves <mariaferraesteves@hotmail.com>
2025-04-11 10:09:15 +0100

    add player functionality

--- game.js
+++ game.js
@@ -1,1 +1,42 @@
-console.log
+class Player{
+    constructor(){
+        this.positionX= 20;
+        this.positionY= 0;
+        this.width=10;
+        this.height=10;
+        this.updateUI();
+
+    }
+    updateUI(){
+        const playerElm=document.getElementById("player");
+        playerElm.style.left = `${this.positionX}vw`;
+        playerElm.style.bottom= `${this.positionY}vh`
+        playerElm.style.width= `${this.width}vw`;
+        playerElm.style.height= `${this.height}vw`;
+    }
+
+    moveLeft(){
+        this.positionX--;
+        this.updateUI();
+    
+    }
+
+
+    moveRight(){
+        this.positionX++;
+        this.updateUI();
+
+    }
+}
+
+const player = new Player()
+
+document.addEventListener("keydown", (e)=>{
+    if (e.code=== 'ArrowLeft'){
+        player.moveLeft();
+    }
+    else if (e.code==='ArrowRight'){
+        player.moveRight();
+    }
+
+})
