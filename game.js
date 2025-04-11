7568fbb9055ef38d3847f95193478a60a2499a0b
Mariaesteves <mariaferraesteves@hotmail.com>
2025-04-11 13:14:45 +0100

    add functionality to generate obstacles

--- game.js
+++ game.js
@@ -1,18 +1,19 @@
 class Player{
     constructor(){
+        this.width=10;
+        this.height=5;
         this.positionX= 20;
         this.positionY= 0;
-        this.width=10;
-        this.height=10;
         this.updateUI();
 
     }
     updateUI(){
         const playerElm=document.getElementById("player");
-        playerElm.style.left = `${this.positionX}vw`;
-        playerElm.style.bottom= `${this.positionY}vh`
         playerElm.style.width= `${this.width}vw`;
         playerElm.style.height= `${this.height}vw`;
+        playerElm.style.left = `${this.positionX}vw`;
+        playerElm.style.bottom= `${this.positionY}vh`
+    
     }
 
     moveLeft(){
@@ -29,7 +30,56 @@ class Player{
     }
 }
 
+class Obstacle{
+    constructor(){
+        this.width=15;
+        this.height=5;
+        this.positionX= Math.floor(Math.random()*(100-this.width+1));
+        this.positionY= 100;
+        this.obstacleElm= null;
+        this.creatDomElement();
+        this.updateUI();
+
+    }
+    creatDomElement(){
+        this.obstacleElm= document.createElement("div")
+        this.obstacleElm.className= "obstacle"
+        const parentElm= document.getElementById("board")
+        parentElm.appendChild(this.obstacleElm)
+    }
+    updateUI(){
+        //const ObstacleElm=document.getElementsByClassName("obstacle");
+        this.obstacleElm.style.width= `${this.width}vw`;
+        this.obstacleElm.style.height= `${this.height}vw`;
+        this.obstacleElm.style.left = `${this.positionX}vw`;
+        this.obstacleElm.style.bottom= `${this.positionY}vh`
+        
+    }
+
+    moveDown(){
+        this.positionY --;
+        this.updateUI();
+    }
+    
+}
+
 const player = new Player()
+const obstacleArr= [];
+
+setInterval(() => {
+   const newObstacle = new Obstacle();
+   obstacleArr.push(newObstacle);
+}, 3000);
+
+
+setInterval(()=>{
+    obstacleArr.forEach((obstacleInstance)=> {
+        obstacleInstance.moveDown();
+
+    })
+}, 50)
+
+
 
 document.addEventListener("keydown", (e)=>{
     if (e.code=== 'ArrowLeft'){
